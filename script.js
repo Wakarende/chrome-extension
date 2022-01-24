f3a55739cd171715a60ea542077ef6c566ac25b3
Joy Kirii <joykirii@gmail.com>
2022-01-24 10:43:56 +0300

    create function that renders out input values

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 'use strict';
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 // Input btn 
 const inputBtn = document.getElementById("input-btn");
 // input 
-const inputEl = document.getElementById("input-el").value;
+const inputEl = document.getElementById("input-el");
 
 // list 
 const ulEl = document.getElementById("ul-el");
 
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+
 // function that saves input to array 
 inputBtn.addEventListener("click", function(){
-  myLeads.push(inputEl);
-  console.log(myLeads);
+  myLeads.push(inputEl.value);
+  renderLeads();
 });
 
-
+// Function that renders out the input values 
+function renderLeads(){
+  let listItems = " ";
 // For loop that logs/renders out input values
-for (let i = 0; i < myLeads.length; i++){
-  ulEl.innerHTML+= "<li>" + myLeads[i] + "</li>"
+  for (let i = 0; i < myLeads.length; i++){
+  listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
 }
+
+
+// Rendering out input values 
+// 1. Create element
+//2. set text content
+// 3. append to ul
+// for (let i = 0; i < myLeads.length; i++) {
+//   const li = document.createElement("li")
+//   li.textContent = myLeads[i]
+//   ulEl.append(li)
+// }
 
 
