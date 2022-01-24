a2bbad3db65ea45cb5071036decc3cd800065ae3
Joy Kirii <joykirii@gmail.com>
2022-01-24 09:44:11 +0300

    create function the saves input values into an array

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let myLeads = [];
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+
+// function saveInput(){
+//   inputBtn = console.log("button clicked");
+// }
+
+inputBtn.addEventListener("click", function(){
+  console.log("Button clicked!");
+  myLeads.push("www.awesomelead.com");
+  console.log(myLeads);
+});
+
+
+
